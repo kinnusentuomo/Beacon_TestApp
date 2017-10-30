68297fc6396cfbd91083097f706ff647c6c59184
Tuomo Kinnunen <tuomo95@gmail.com>
2017-10-30 09:14:10 +0200

    Lisätty opettajan koodit + korjaukset

--- app/src/main/java/tuomomees/beacontestapp/MainActivity.java
+++ app/src/main/java/tuomomees/beacontestapp/MainActivity.java
@@ -1,5 +1,6 @@
 package tuomomees.beacontestapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +53,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //ADDED
+    public static void calculate(Context tama) {
+        ArrayList<Integer> taulu = new ArrayList<Integer>();
+        int luku = 9;
+        while (luku != 0) {
+            taulu.add(luku);
+            luku = luku - 1;
+            String aString = Integer.toString(luku);
+            Toast.makeText(tama/*getApplicationContext()*/, "luku : " + aString, Toast.LENGTH_LONG).show();
+        }
+        int isum = summa(taulu); //lasketaan summa
+        String aSum = Integer.toString(isum);
+        Toast.makeText(tama/*getApplicationContext()*/, "summa : " + aSum, Toast.LENGTH_LONG).show();
+    }
+
+    private static int summa(ArrayList<Integer> tau) {
+        int summa = 0;
+        for (int i = 0; i < tau.size(); ++i) // for-each olisi tÃ¤ssÃ¤ toki luontevampi,
+            summa += tau.get(i);
+        return summa;
+    }
+
+    public static void hashMapEx(Context tama) {
+        //HashMap<K,V>
+        //Class HashMap object is a construct, that has K type values, like "key"), they are associated with V type values, like "value".
+        //Also by the "key" is seached the value connected to this "key".
+        //In a way HashMap object is like a table, that is indexed by K type values (not with numbers).
+
+        HashMap<String, ArrayList<String>> who =
+                new HashMap<String, ArrayList<String>>();
+
+        ArrayList<String> maijaStuff = new ArrayList<String>();
+        maijaStuff.add("doll");
+        maijaStuff.add("bubby");
+        maijaStuff.add("star");
+
+        ArrayList<String> pekkaStuff = new ArrayList<String>();
+        pekkaStuff.add("car");
+        pekkaStuff.add("measure");
+        pekkaStuff.add("sword");
+
+        who.put("Maija", maijaStuff);
+        who.put("Pekka", pekkaStuff);
+
+        Toast.makeText(tama/*getApplicationContext()*/, "whose: " + who, Toast.LENGTH_LONG).show();
+        //System.out.println(who);
+        Toast.makeText(tama, "whose/Pekka: " + who.get("Pekka"), Toast.LENGTH_LONG).show();
+        Toast.makeText(tama, "whose/Maija(2): " + who.get("Maija").get(2), Toast.LENGTH_LONG).show();
     }
 }
